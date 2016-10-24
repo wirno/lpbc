486fbba7b0c127c0fc761df8d4babc63da9ee560
Matthieu Livet <matthieu@lesgrappes.com>
2016-10-24 15:32:19 +0200

    update wp-config

--- wp-config.php
+++ wp-config.php
@@ -19,19 +19,19 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'lpbc');
+define('DB_NAME', '');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', '');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'lesgrappes');
+define('DB_PASSWORD', '');
 
 /** Adresse de l'hébergement MySQL. */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** Type de collation de la base de données.
   * N'y touchez que si vous savez ce que vous faites.
